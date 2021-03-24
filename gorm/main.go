75c0fce30240adf214ec3f4e49105cef42ca18e7
Augustu Scofield <The.Augustu.Scofield@gmail.com>
2021-03-24 11:19:51 +0800

    add value struct

--- gorm/main.go
+++ gorm/main.go
@@ -23,7 +23,6 @@ type Event struct {
 }
 
 func main() {
-	// dsn := "root@tcp(127.0.0.1:13306)/rank?charset=utf8mb4&parseTime=True&loc=Local"
 	dsn := "root:123456@tcp(127.0.0.1:3306)/rank?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		NamingStrategy: naming.TimeNamingStrategy{
