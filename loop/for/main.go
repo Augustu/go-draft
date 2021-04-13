47cbe406935fbfb56af7bd2c714e05aa83c97757
Augustu Scofield <The.Augustu.Scofield@gmail.com>
2021-04-13 21:07:23 +0800

    add for test

--- loop/for/main.go
+++ loop/for/main.go
@@ -7,4 +7,15 @@ func main() {
 	for i := 0; i < total; i++ {
 		fmt.Println("i = ", i)
 	}
+
+	list := []string{"a", "b", "c"}
+	for idx, l := range list {
+		fmt.Println(idx, l)
+		if l == "b" {
+			fmt.Println("continue", idx, l)
+			continue
+		}
+		fmt.Println(idx, l)
+	}
+
 }
