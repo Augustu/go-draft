e99dc0c03138f202e286ef6a826cdae9e636b870
Augustu Scofield <The.Augustu.Scofield@gmail.com>
2021-05-25 18:48:52 +0800

    orderedlist with struct

--- redis/orderedlist/main.go
+++ redis/orderedlist/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"strconv"
 	"time"
 
 	"github.com/go-redis/redis/v8"
@@ -14,20 +16,46 @@ type client struct {
 	context context.Context
 }
 
+type sa struct {
+	A int    `json:"a"`
+	B string `json:"b"`
+}
+
 func (c *client) put(key string, total int) {
 	for i := 0; i < total; i++ {
-		c.RPush(c.context, key, i)
+
+		b, err := json.Marshal(sa{
+			A: i,
+			B: strconv.Itoa(i),
+		})
+		if err != nil {
+			log.Println(err)
+		}
+
+		_, err = c.RPush(c.context, key, string(b)).Result()
+		if err != nil {
+			log.Println(err)
+		}
 	}
 }
 
 func (c *client) get(key string) {
 	for {
+		var ss sa
 		res, err := c.BLPop(c.context, time.Second, key).Result()
 		if err != nil {
 			log.Println(err)
 			break
 		}
-		fmt.Println(res)
+
+		s := res[1]
+		err = json.Unmarshal([]byte(s), &ss)
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		fmt.Println(ss)
 	}
 }
 
